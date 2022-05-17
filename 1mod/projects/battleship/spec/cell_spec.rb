ca41bce3b14079b0367f542556a74a5febeede48
Justin Ramirez <jusrez528@gmail.com>
2022-05-16 21:45:45 -0400

    Add test to confirm ship is not in cell

--- 1mod/projects/battleship/spec/cell_spec.rb
+++ 1mod/projects/battleship/spec/cell_spec.rb
@@ -13,4 +13,10 @@ RSpec.describe Cell do
 
 		expect(cell.coordinate).to eq("B4")
 	end
+
+	it 'confirms ship is not in cell' do
+		cell = Cell.new("B4")
+
+		expect(cell.ship).to eq(nil)
+	end
 end
