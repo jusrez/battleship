c8ff57a6ad8281fe0dfc990f121b035f19156c8a
Jared Hardinger <jaredhardinger@gmail.com>
2022-05-16 23:58:11 -0400

    Define methods so that testspass

--- 1mod/projects/battleship/lib/ship.rb
+++ 1mod/projects/battleship/lib/ship.rb
@@ -1,21 +1,22 @@
 class Ship
 
-attr_reader :name, :length
+attr_reader :name, :length, :number_of_hits
 
   def initialize(name, length)
     @name = name
     @length = length
+    @number_of_hits = []
   end
 
   def health
-
+    length - number_of_hits.count
   end
 
   def sunk?
-
+    health < 1
   end
 
   def hit
-
+    number_of_hits << "hit!"
   end
-  
+end
