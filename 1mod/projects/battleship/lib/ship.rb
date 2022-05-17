d757e62057f6e976baf2335ef87a0e493bd6a80b
Jared Hardinger <jaredhardinger@gmail.com>
2022-05-16 23:19:45 -0400

    Create framework for tests needed

---
+++ 1mod/projects/battleship/lib/ship.rb
@@ -0,0 +1,21 @@
+class Ship
+
+attr_reader :name, :length
+
+  def initialize(name, length)
+    @name = name
+    @length = length
+  end
+
+  def health
+
+  end
+
+  def sunk?
+
+  end
+
+  def hit
+
+  end
+  
