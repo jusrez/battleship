1e374f99a9b13eb03c217bc53c7bae0e7b1a77a8
Jared Hardinger <jaredhardinger@gmail.com>
2022-05-17 21:21:54 -0400

    Move variables back inside it blocks

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -1,26 +1,28 @@
 require './lib/ship'
 
 RSpec.describe Ship do
-
-  cruiser = Ship.new("Cruiser", 3)
-
   it 'has name' do
+    cruiser = Ship.new("Cruiser", 3)
     expect(cruiser.name).to eq("Cruiser")
   end
 
   it 'has length' do
+    cruiser = Ship.new("Cruiser", 3)
     expect(cruiser.length).to eq(3)
   end
 
   it 'has health' do
+    cruiser = Ship.new("Cruiser", 3)
     expect(cruiser.health).to eq(3)
   end
 
   it 'has sunk?' do
+    cruiser = Ship.new("Cruiser", 3)
     expect(cruiser.sunk?).to eq(false)
   end
 
   it 'has reduced health after hit' do
+    cruiser = Ship.new("Cruiser", 3)
     cruiser.hit
 
     expect(cruiser.health).to eq(2)
@@ -31,6 +33,7 @@ RSpec.describe Ship do
   end
 
   it 'has sunk' do
+    cruiser = Ship.new("Cruiser", 3)
     expect(cruiser.sunk?).to eq(false)
 
     cruiser.hit
