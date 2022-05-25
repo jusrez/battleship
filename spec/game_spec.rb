5d044582126b24f6791277f6773865cbb4d67006
Jared Hardinger <jaredhardinger@gmail.com>
2022-05-25 16:40:43 -0400

    Add tests

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -7,15 +7,31 @@ RSpec.describe Game do
 
 	it 'exists' do
 	 game = Game.new
-
 	 expect(game).to be_instance_of(Game)
+	 expect(game.computer_cruiser).to be_instance_of(Ship)
+	 expect(game.computer_submarine).to be_instance_of(Ship)
+	 expect(game.player_cruiser).to be_instance_of(Ship)
+	 expect(game.player_submarine).to be_instance_of(Ship)
 	end
 
-	it 'randomly places computers ships' do
+	it 'starts with unsunk fleets' do
 		game = Game.new
-		game.computer_setup
+		expect(game.computer_fleet_sunk?).to be(false)
+		expect(game.player_fleet_sunk?).to be(false)
+	end
 
-		expect(game.computer_board.cells.all? {|cell, cell_instance| cell_instance.empty?}).to eq(false)
+	it 'places a computer submarine' do
+		game = Game.new
+		expect(game.submarine_computer.empty?).to eq(true)
+		game.computer_place_submarine
+		expect(game.submarine_computer.count).to eq(2)
+	end
+
+	it 'places a computer cruiser' do
+		game = Game.new
+		expect(game.cruiser_computer.empty?).to eq(true)
+		game.computer_place_cruiser
+		expect(game.cruiser_computer.count).to eq(3)
 	end
 
 end
